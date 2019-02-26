ce933024a58538e3f80b3e5837a8dd07b78b6e92
김동석 <kim@virtualive.co.kr>
2019-02-26 22:15:22 +0900

    + 리뉴얼 소스 업데이트 + v1.0 버전

--- WebEwelfare/WebEwelfare/AppDelegate.swift
+++ WebEwelfare/WebEwelfare/AppDelegate.swift
@@ -2,30 +2,14 @@
 //  AppDelegate.swift
 //  WebEwelfare
 //
-//  Created by nam yeon hun on 28/12/2018.
-//  Copyright © 2018 nam yeon hun. All rights reserved.
+//  Created by 김동석 on 19/02/2019.
+//  Copyright © 2019 nam yeon hun. All rights reserved.
 //
 
 import UIKit
 import UserNotifications
 
 import Firebase
-
-
-enum Notifications {
-    enum Categories: String {
-        case Message
-    }
-    enum Actions: String {
-        case RemindMeLater
-        case Reply
-    }
-}
-
-enum Key {
-    static let fcm = "fcm"
-    static let token = "token"
-}
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -36,23 +20,54 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        window = UIWindow()
-        window?.backgroundColor = .white
-        window?.rootViewController = MainWebViewController()
-        
-        window?.makeKeyAndVisible()
         
         FirebaseApp.configure()
         
-        // [START set_messaging_delegate]
-        Messaging.messaging().delegate = self
+        // push
+        registerForPushNotifications(application)
         
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+    // Called when APNs has assigned the device a unique token
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        // Convert token to string (디바이스 토큰 값을 가져옵니다.)
+        let deviceTokenString = deviceToken.reduce("", {$0 + String(format: "%02X", $1)})
+        
+        // Print it to console(토큰 값을 콘솔창에 보여줍니다. 이 토큰값으로 푸시를 전송할 대상을 정합니다.)
+        print("APNs device token: \(deviceTokenString)")
+        
+        Messaging.messaging().apnsToken = deviceToken
+        
+        // Persist it in your backend in case it's new
+    }
+    
+    private func registerForPushNotifications(_ application: UIApplication) {
+        Messaging.messaging().delegate = self
         Messaging.messaging().shouldEstablishDirectChannel = true
         
-        // [END set_messaging_delegate]
-        // Register for remote notifications. This shows a permission dialog on first run, to
-        // show the dialog at a more appropriate time move this registration accordingly.
-        // [START register_for_notifications]
         if #available(iOS 10.0, *) {
             // For iOS 10 display notification (sent via APNS)
             UNUserNotificationCenter.current().delegate = self
@@ -68,67 +83,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         
         application.registerForRemoteNotifications()
-        
-        // [END register_for_notifications]
-        
-        // [END register_for_notifications]
-        
-        return true
     }
     
-    // [START receive_message]
-    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
-        // If you are receiving a notification message while your app is in the background,
-        // this callback will not be fired till the user taps on the notification launching the application.
-        // TODO: Handle data of notification
-        // With swizzling disabled you must let Messaging know about the message, for Analytics
-        // Messaging.messaging().appDidReceiveMessage(userInfo)
-        // Print message ID.
-        if let messageID = userInfo[gcmMessageIDKey] {
-            print("Message ID: \(messageID)")
-        }
-        
-        // Print full message.
-        print(userInfo)
-    }
-    
-    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any],
-                     fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-        // If you are receiving a notification message while your app is in the background,
-        // this callback will not be fired till the user taps on the notification launching the application.
-        // TODO: Handle data of notification
-        // With swizzling disabled you must let Messaging know about the message, for Analytics
-        // Messaging.messaging().appDidReceiveMessage(userInfo)
-        // Print message ID.
-        if let messageID = userInfo[gcmMessageIDKey] {
-            print("Message ID: \(messageID)")
-        }
-        
-        // Print full message.
-        print(userInfo)
-        
-        completionHandler(UIBackgroundFetchResult.newData)
-    }
-    // [END receive_message]
-    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
-        print("Unable to register for remote notifications: \(error.localizedDescription)")
-    }
-    
-    // This function is added here only for debugging purposes, and can be removed if swizzling is enabled.
-    // If swizzling is disabled then this function must be implemented so that the APNs token can be paired to
-    // the FCM registration token.
-    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        print("APNs token retrieved: \(deviceToken)")
-        
-        // With swizzling disabled you must set the APNs token here.
-        // Messaging.messaging().apnsToken = deviceToken
-    }
 }
 
-// [START ios_10_message_handling]
+
 @available(iOS 10, *)
 extension AppDelegate : UNUserNotificationCenterDelegate {
-    //포그라운드 에서 ㄴ티가 왔을때 발생하는 메서드
+    
+    //포그라운드 에서 노티가 왔을때 발생하는 메서드
     // Receive displayed notifications for iOS 10 devices.
     func userNotificationCenter(_ center: UNUserNotificationCenter,
                                 willPresent notification: UNNotification,
@@ -137,6 +100,7 @@ extension AppDelegate : UNUserNotificationCenterDelegate {
         
         // With swizzling disabled you must let Messaging know about the message, for Analytics
         // Messaging.messaging().appDidReceiveMessage(userInfo)
+        
         // Print message ID.
         if let messageID = userInfo[gcmMessageIDKey] {
             print("Message ID: \(messageID)")
@@ -146,10 +110,10 @@ extension AppDelegate : UNUserNotificationCenterDelegate {
         print(userInfo)
         
         // Change this to your preferred presentation option
-        completionHandler([UNNotificationPresentationOptions.alert,UNNotificationPresentationOptions.sound,UNNotificationPresentationOptions.badge])
+        completionHandler([UNNotificationPresentationOptions.alert, UNNotificationPresentationOptions.sound, UNNotificationPresentationOptions.badge])
     }
     
-    //백그라운드에서 노티를 눌렀을때 반응하는 메서드
+    // 백그라운드에서 노티를 눌렀을때 반응하는 메서드
     func userNotificationCenter(_ center: UNUserNotificationCenter,
                                 didReceive response: UNNotificationResponse,
                                 withCompletionHandler completionHandler: @escaping () -> Void) {
@@ -164,26 +128,28 @@ extension AppDelegate : UNUserNotificationCenterDelegate {
         
         completionHandler()
     }
+    
 }
-// [END ios_10_message_handling]
+
 
 extension AppDelegate : MessagingDelegate {
-    // [START refresh_token]
+    
     func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String) {
         print("Firebase registration token: \(fcmToken)")
-        UserDefaults().set(fcmToken, forKey: Key.fcm)
+        
+        AppManager.instance.fcmToken = fcmToken
+        
         let dataDict:[String: String] = ["token": fcmToken]
         NotificationCenter.default.post(name: Notification.Name("FCMToken"), object: nil, userInfo: dataDict)
         // TODO: If necessary send token to application server.
         // Note: This callback is fired at each app startup and whenever a new token is generated.
     }
-    // [END refresh_token]
-    // [START ios_10_data_message]
+    
     // Receive data messages on iOS 10+ directly from FCM (bypassing APNs) when the app is in the foreground.
     // To enable direct data messages, you can set Messaging.messaging().shouldEstablishDirectChannel to true.
     func messaging(_ messaging: Messaging, didReceive remoteMessage: MessagingRemoteMessage) {
         print("Received data message: \(remoteMessage.appData)")
         
     }
-    // [END ios_10_data_message]
+    
 }
---
+++ WebEwelfare/WebEwelfare/scene/other/OtherWebVController.swift
@@ -0,0 +1,274 @@
+//
+//  OtherWebVController.swift
+//  WebEwelfare
+//
+//  Created by 김동석 on 19/02/2019.
+//  Copyright © 2019 nam yeon hun. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class OtherWebVController: BaseVController {
+    
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var lbTitle: UILabel!
+    
+    private var wkWebView: WKWebView!
+    private var newWkWebView: WKWebView?
+
+    var shopUrl: String!
+    var titleText: String?
+    
+    override func loadView() {
+        super.loadView()
+        
+        wkWebView = WKWebView(frame: containerView.frame, configuration: getWebViewConfiguration())
+        wkWebView.uiDelegate = self
+        wkWebView.navigationDelegate = self
+        wkWebView.scrollView.delegate = self
+        
+        containerView.addSubview(wkWebView)
+        
+        wkWebView.translatesAutoresizingMaskIntoConstraints = false
+        view.addConstraint(NSLayoutConstraint(item: wkWebView, attribute: .leading, relatedBy: .equal, toItem: containerView, attribute: .leading, multiplier: 1, constant: 0))
+        view.addConstraint(NSLayoutConstraint(item: wkWebView, attribute: .trailing, relatedBy: .equal, toItem: containerView, attribute: .trailing, multiplier: 1, constant: 0))
+        view.addConstraint(NSLayoutConstraint(item: wkWebView, attribute: .top, relatedBy: .equal, toItem: containerView, attribute: .top, multiplier: 1, constant: 0))
+        view.addConstraint(NSLayoutConstraint(item: wkWebView, attribute: .bottom, relatedBy: .equal, toItem: containerView, attribute: .bottom, multiplier: 1, constant: 0))
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        initView()
+        
+        loadUrl(urlPath: shopUrl)
+    }
+    
+    @IBAction func onClickBack(_ sender: UIButton) {
+        if wkWebView.canGoBack {
+            wkWebView.goBack()
+        }
+    }
+    
+    @IBAction func onClickClose(_ sender: UIButton) {
+        let alertController = UIAlertController(title: "e복지", message: "e복지 페이지로 돌아가시겠습니까?", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "취소", style: .default, handler: { (action) in
+        }))
+        alertController.addAction(UIAlertAction(title: "확인", style: .default, handler: { (action) in
+            self.dismiss(animated: false, completion: nil)
+        }))
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    private func initView() {
+        if titleText != nil {
+            lbTitle.text = titleText!
+        }
+    }
+    
+    private func getWebViewConfiguration() -> WKWebViewConfiguration {
+        let config = WKWebViewConfiguration()
+        let contentController = WKUserContentController()
+        
+//        let userScript = WKUserScript(
+//            source: "sendLoginAction()",
+//            injectionTime: WKUserScriptInjectionTime.atDocumentEnd,
+//            forMainFrameOnly: true
+//        )
+//        contentController.addUserScript(userScript)
+//        contentController.add(self, name:"sendLoginAction")
+//        config.userContentController = contentController
+        
+        let preferences = WKPreferences()
+        preferences.javaScriptCanOpenWindowsAutomatically = true
+        preferences.javaScriptEnabled = true
+        config.preferences = preferences
+        
+        return config
+    }
+    
+    func loadUrl(urlPath: String) {
+        guard let url = URL(string: urlPath) else {
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        
+        var cookies = HTTPCookie.requestHeaderFields(with: HTTPCookieStorage.shared.cookies(for: request.url!)!)
+        if let value = cookies["Cookie"] {
+            request.addValue(value, forHTTPHeaderField: "Cookie")
+        }
+        
+        wkWebView.load(request)
+    }
+
+}
+
+
+extension OtherWebVController: WKUIDelegate {
+    
+    func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void) {
+        let alertController = UIAlertController(title: "e복지", message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "확인", style: .default, handler: { (action) in
+            completionHandler()
+        }))
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func webView(_ webView: WKWebView, runJavaScriptConfirmPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (Bool) -> Void) {
+        let alertController = UIAlertController(title: "e복지", message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "확인", style: .default, handler: { (action) in
+            completionHandler(true)
+        }))
+        alertController.addAction(UIAlertAction(title: "취소", style: .default, handler: { (action) in
+            completionHandler(false)
+        }))
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func webView(_ webView: WKWebView, runJavaScriptTextInputPanelWithPrompt prompt: String, defaultText: String?, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (String?) -> Void) {
+        let alertController = UIAlertController(title: "e복지", message: prompt, preferredStyle: .alert)
+        alertController.addTextField { (textField) in
+            textField.text = defaultText
+        }
+        alertController.addAction(UIAlertAction(title: "확인", style: .default, handler: { (action) in
+            if let text = alertController.textFields?.first?.text {
+                completionHandler(text)
+            }
+            else {
+                completionHandler(defaultText)
+            }
+        }))
+        alertController.addAction(UIAlertAction(title: "취소", style: .default, handler: { (action) in
+            completionHandler(nil)
+        }))
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+}
+
+
+extension OtherWebVController: WKNavigationDelegate {
+    
+    func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
+        newWkWebView = WKWebView(frame: webView.frame, configuration: configuration)
+        newWkWebView!.scrollView.bounces = self.wkWebView.scrollView.bounces
+        newWkWebView!.uiDelegate = self.wkWebView.uiDelegate
+        newWkWebView!.navigationDelegate = self.wkWebView.navigationDelegate
+        containerView.addSubview(newWkWebView!)
+        return newWkWebView
+    }
+    
+    func webViewDidClose(_ webView: WKWebView) {
+        if webView == newWkWebView! {
+            newWkWebView!.removeFromSuperview()
+            newWkWebView = nil
+        }
+    }
+    
+    // 중복적으로 리로드 방지
+    func webViewWebContentProcessDidTerminate(_ webView: WKWebView) {
+        webView.reload()
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        HTTPCookieStorage.shared.cookieAcceptPolicy = .always
+        
+        // iOS10 신한, 삼성, NH 등 앱카드 관련 ///////////////////
+        let device = UIDevice.current
+        var backgroundSupported = false
+        
+        if device.responds(to: #selector(getter: UIDevice.isMultitaskingSupported)){
+            backgroundSupported = device.isMultitaskingSupported
+        }
+        NSLog("backgroundSupported ==>%@", backgroundSupported ? "YES" : "NO")
+        
+        if !backgroundSupported {
+            let alertController = UIAlertController(title: "e복지", message: "멀티테스킹을 지원하는 기기 또는 어플만 공인인증서비스가 가능합니다.", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "확인", style: .default, handler: { (action) in
+            }))
+            
+            decisionHandler(.allow)
+            return
+        }
+        
+        /*
+        // 스마트 신한앱 다운로드 url
+        let sh_url = "http://itunes.apple.com/us/app/id360681882?mt=8" //신한Mobile앱 결제 다운로드 url
+        let sh_url2 = "https://itunes.apple.com/kr/app/sinhan-mobilegyeolje/id572462317?mt=8"
+         
+        // 현대 다운로드 url
+        let hd_url = "http://itunes.apple.com/kr/app/id362811160?mt=8"
+         
+        // 스마트 신한 url 스키마
+        let sh_appname = "smshinhanansimclick" //스마트 신한앱 url 스키마
+        let sh_appname2 = "shinhan-sr-ansimclick"
+         
+        // 현대카드 url
+        let hd_appname = "smhyundaiansimclick" //현대카드 url
+        let hd_vbv = "ansimclick.hyundaicard.com"
+        */
+        
+        if let urlStr = navigationAction.request.url?.absoluteString {
+            print("요청된 URL ==> \(urlStr)")
+            
+            let nsStringUrlStr = urlStr as NSString
+
+            if Int(nsStringUrlStr.range(of: "ansimclick.hyundaicard.com").location ) != NSNotFound {
+                decisionHandler(.allow)
+                return
+            }
+            else {
+                if Int(nsStringUrlStr.range(of: "http://itunes.apple.com").location) != NSNotFound {
+                    print("1. 앱설치 url 입니다. ==>%@",urlStr)
+                    UIApplication.shared.open(URL(string: urlStr)!, options: [:], completionHandler: nil)
+                    decisionHandler(.cancel)
+                    
+                    if newWkWebView != nil {
+                        webViewDidClose(newWkWebView!)
+                    }
+                    return
+                }
+            }
+
+            if Int(nsStringUrlStr.range(of: "ansimclick").location ) != NSNotFound {
+                print("2. 앱설치 url 입니다. ==>%@",urlStr)
+                UIApplication.shared.open(URL(string: urlStr)!, options: [:], completionHandler: nil)
+                decisionHandler(.cancel)
+                return
+            }
+
+            if Int(nsStringUrlStr.range(of: "appfree").location ) != NSNotFound {
+                print("3. 앱설치 url 입니다. ==>%@",urlStr)
+                UIApplication.shared.open(URL(string: urlStr)!, options: [:], completionHandler: nil)
+                decisionHandler(.cancel)
+                return
+            }
+        }
+        
+        decisionHandler(.allow)
+    }
+    
+}
+
+
+extension OtherWebVController: WKScriptMessageHandler {
+    
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+    }
+    
+}
+
+
+extension OtherWebVController: UIScrollViewDelegate {
+    
+    // disable zooming in webview
+    func scrollViewWillBeginZooming(_ scrollView: UIScrollView, with view: UIView?) {
+        scrollView.pinchGestureRecognizer?.isEnabled = false
+    }
+    
+}
