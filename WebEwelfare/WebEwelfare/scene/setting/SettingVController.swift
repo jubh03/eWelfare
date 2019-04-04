f4f6405d487287a9e5825c814af1e1acc273a77b
김동석 <kim@virtualive.co.kr>
2019-04-04 16:48:01 +0900

    + 설정화면 적용 중

---
+++ WebEwelfare/WebEwelfare/scene/setting/SettingVController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingVController.swift
+//  WebEwelfare
+//
+//  Created by KimDongSeok on 04/04/2019.
+//  Copyright © 2019 nam yeon hun. All rights reserved.
+//
+
+import UIKit
+
+class SettingVController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
